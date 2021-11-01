f7a957c0c9cf028bda878f20548473752dbf5ac0
R Surahutomo Aziz Pradana <pradana@Rs-MacBook-Pro.local>
2021-11-02 06:37:05 +0700

    add feature to compile yaml to compiled component

--- lib/executables/compile-scene.sh
+++ lib/executables/compile-scene.sh
@@ -1,5 +1,8 @@
 initiatedProjectPath=`pwd`
 echo Compiling the project ...
+
+payload=( "$@" )
 cd $(npm root -g)/astrophel-cli/lib
-node $(npm root -g)/astrophel-cli/node_modules/hygen/dist/bin.js scene-compiler scenes --initiatedProjectPath=$initiatedProjectPath --filename=$filename $payload
+node $(npm root -g)/astrophel-cli/node_modules/hygen/dist/bin.js scene-compiler scenes --initiatedProjectPath=$initiatedProjectPath "${payload[@]//\%20/ }"
+
 echo Project successfully compiled
